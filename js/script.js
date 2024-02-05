adf7d8fdbfe046470ad4e534206208967f97adfd
Sohel <152502004+5OH3L@users.noreply.github.com>
2024-02-05 13:09:05 +0530

    Added features and cleaned some bug
    Now you can use different values for row and column and I've fixed wrong input popup

--- js/script.js
+++ js/script.js
@@ -12,49 +12,108 @@ let randomNum255 = () => Math.floor((Math.random()*255)+1);
 let makeGrid = (gridSize) => {
     container.innerHTML = "";
     container.className = 'border2px';
-    for (let i = 0; i < +gridSize; i++){
-        let row = document.createElement('div');
-        row.className = "row";
-        for (let j = 0; j < +gridSize; j++){
-            let column = document.createElement('div');
-            column.className = "column";
-            column.addEventListener('mouseover', (e)=>{
-                if (e.buttons == 1 || e.button == 1){
+    if (typeof(gridSize) == 'object'){
+        for (let i = 0; i < +gridSize[1]; i++){
+            let row = document.createElement('div');
+            row.className = "row";
+            for (let j = 0; j < +gridSize[0]; j++){
+                let column = document.createElement('div');
+                column.className = "column";
+                column.addEventListener('mouseover', (e)=>{
+                    if (e.buttons == 1 || e.button == 1){
+                        e.target.style.backgroundColor = `rgb(${randomNum255()},${randomNum255()},${randomNum255()})`;
+                    }
+                });
+                column.addEventListener('mousedown', (e)=>{
                     e.target.style.backgroundColor = `rgb(${randomNum255()},${randomNum255()},${randomNum255()})`;
-                }
-            });
-            column.addEventListener('mousedown', (e)=>{
-                e.target.style.backgroundColor = `rgb(${randomNum255()},${randomNum255()},${randomNum255()})`;
-            });
-            row.appendChild(column);
+                });
+                row.appendChild(column);
+            }
+            container.appendChild(row);
         }
-        container.appendChild(row);
+    }
+    else{
+        for (let i = 0; i < +gridSize; i++){
+            let row = document.createElement('div');
+            row.className = "row";
+            for (let j = 0; j < +gridSize; j++){
+                let column = document.createElement('div');
+                column.className = "column";
+                column.addEventListener('mouseover', (e)=>{
+                    if (e.buttons == 1 || e.button == 1){
+                        e.target.style.backgroundColor = `rgb(${randomNum255()},${randomNum255()},${randomNum255()})`;
+                    }
+                });
+                column.addEventListener('mousedown', (e)=>{
+                    e.target.style.backgroundColor = `rgb(${randomNum255()},${randomNum255()},${randomNum255()})`;
+                });
+                row.appendChild(column);
+            }
+            container.appendChild(row);
+        }
     }
 }
 
 makeGrid(10)
 
 button.addEventListener('click', (e) => {
-    let input = prompt("» Enter the grid layout:");
-    // if (typeof(input) == "string"){
-    //     split = input.split('x');
-    //     if (split.length != 2){
-    //         return;
-    //     }else{
-    //         x = split[0];
-    //         y = split[1];
-    //         console.log(x,y);
-    //     };
-    // };
-    while (+input > 100 || +input < 0){
-        input = +prompt("The limit is from 1x1 to 100x100!");
-    }
-    if (input == null || input == undefined || input == ""){
+    let input = prompt("» Enter the grid layout:", "How many dads do you have ?");
+    while(true){
+        if(isNaN(input)){
+            let newInput = input.split('x');
+            if(newInput.length == 2 && !isNaN(newInput[0]) && !isNaN(newInput[1])){
+                gridShow.textContent = `${+newInput[0]}x${+newInput[1]}`;
+                makeGrid(newInput);
+                break;
+            }else{
+                input = prompt("» Invalid Input:", "Are you dumb?");
+            }
+        }else{
+            if(!isNaN(input) && input > 0 && input <= 1000){
+                gridShow.textContent = `${+input}x${+input}`;
+                makeGrid(input);
+                break;
+            }else{
+                input = prompt("» Out of range ( 1 - 1000 ):", "If your can handle Ofcourse :]");
+            }
+        }
+    };
 
-    }else{
-        gridShow.textContent = `${input}x${input}`;
-        makeGrid(input);
-    }
+    //              Old conditional statement
+    // if(isNaN(+input)){
+    //     let newInput = input.split('x');
+    //     if(newInput.length == 2){
+    //         gridShow.textContent = `${+newInput[0]}x${+newInput[1]}`;
+    //         makeGrid(newInput);
+    //     }else{
+    //         while(true){
+    //             input = prompt("» Enter the grid layout:", "Only 1 or 2 arguments you dumb?");
+    //             if (isNaN(input)){
+    //                 newInput = input.split('x');
+    //                 gridShow.textContent = `${+newInput[0]}x${+newInput[1]}`;
+    //                 makeGrid(newInput);
+    //                 loop = 0;
+    //             }else{
+    //                 gridShow.textContent = `${input}x${input}`;
+    //                 makeGrid(input);
+    //                 loop = 0;
+    //             }
+    //         }
+    //         gridShow.textContent = `${+newInput[0]}x${+newInput[1]}`;
+    //         makeGrid(newInput);
+    //     }
+
+    // }else{
+    //     while (+input >= 1000 || +input < 0){
+    //         input = prompt("The limit is from 1x1 to 1000x1000 (If you pc can handle)!");
+    //     }
+    //     if (input == null || input == undefined || input == ""){
+    
+    //     }else if (typeof(+input) == "number"){
+    //         gridShow.textContent = `${input}x${input}`;
+    //         makeGrid(input);
+    //     }
+    // }
 });
 
 toggleBorder.addEventListener('click', () =>{
